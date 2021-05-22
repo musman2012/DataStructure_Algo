62ea1dab2a4f6deeb3c5e8a9ff70138d745b0925
Muhammad Usman <m.usman.uob@gmail.com>
2021-05-22 19:03:04 +0100

    added testing code

--- BinarySearch.py
+++ BinarySearch.py
@@ -13,3 +13,17 @@ def search_num(start, end, array, target):
         return search_num(start, mid - 1, array, target)
     elif number < target:
         return search_num(mid + 1, end, array, target)
+    
+    
+my_arr = [100,200,300,400,500,600]
+my_tar,my_tar2,my_tar3  = 200,500,303
+print(search_num(0, 5, my_arr, my_tar))
+print(search_num(0, 5, my_arr, my_tar2))
+print(search_num(0, 5, my_arr, my_tar3))
+
+'''
+Output:
+1
+4
+-99
+'''
