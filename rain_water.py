d75fa321eea7bba18124b28ed15f30d5de3b3f7f
Muhammad Usman <m.usman.uob@gmail.com>
2021-06-12 19:10:13 +0100

    Create rain_water.py

---
+++ rain_water.py
@@ -0,0 +1,24 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        total_vals = len(height)
+        water = 0
+        
+        if total_vals == 0:
+            return water
+        
+        left_max = [0]*total_vals
+        right_max = [0]*total_vals
+        
+        left_max[0] = height[0]
+        right_max[total_vals - 1] = height[total_vals - 1]
+        
+        for i in range(1, total_vals):
+            left_max[i] = max(height[i], left_max[i-1])
+            
+        for i in range(total_vals - 2, -1, -1):
+            right_max[i] = max(height[i], right_max[i+1])
+            
+        for i in range(0, total_vals):
+            water += min(right_max[i], left_max[i]) - height[i]
+            
+        return water
